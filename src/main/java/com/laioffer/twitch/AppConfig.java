e92bece4a68150e94a085a2469da2650f0f61be6
Pengrui Fan <pengrui_f@Pengruis-MacBook-Pro.local>
2023-12-08 12:43:56 -0500

    create register body

--- src/main/java/com/laioffer/twitch/AppConfig.java
+++ src/main/java/com/laioffer/twitch/AppConfig.java
@@ -3,10 +3,26 @@ package com.laioffer.twitch;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,4 +34,3 @@ public class AppConfig {
         return http.build();
     }
 }
-
